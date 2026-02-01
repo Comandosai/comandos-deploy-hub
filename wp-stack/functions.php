aaf8436158786200700cf9be2d162eb14403f1be
root <root@wnwmutgkfj.local>
2026-02-01 17:06:56 +0000

    Customizer Fix v5.6: Linked Critical CSS with dynamic variables and adjusted injection priority to -110.

--- wp-stack/functions.php
+++ wp-stack/functions.php
@@ -492,7 +492,7 @@ add_action('wp_head', function () {
         body { background-color: var(--white) !important; }
     </style>
     <?php
-}, 0);
+}, -110);
 
 // ИНЪЕКЦИЯ ASPECT-RATIO (FIX CLS для 'Original' и Custom)
 add_filter('wp_get_attachment_image_attributes', function($attr, $attachment, $size) {
